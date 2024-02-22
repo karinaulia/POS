d4987a6da5d17b73a9e6abe251f4a786dc65d942
Karina Aulia Primanti Putri <karinauliap@gmail.com>
2024-02-22 16:11:49 +0700

    first commit

---
+++ resources/views/home/home.blade.php
@@ -0,0 +1,5 @@
+<!DOCTYPE html>
+<html>
+    <h1>Selamat Datang</h1>
+    <h2>Ini adalah website POS (Point of Sales)</h2>
+</html>
---
+++ resources/views/product/foodBeverages.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <h1>Food Beverages</h1>
+    <ol>
+        <li>Nasi Goreng</li>
+        <li>Mie Goreng</li>
+        <li>Ayam Bakar</li>
+        <li>Soto Ayam</li>
+    </ol>
+</html>
